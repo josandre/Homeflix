43b359b1b7451683dea5eee78229617c37bfcd9a
Joshua Bolanos <jbolanosh@ucenfotec.ac.cr>
2022-07-12 04:52:01 -0600

    Creacion del login

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller{
+    //Variables de javafx
+    @FXML
+    private Label loginMessageLabel;
+    @FXML
+    private Button cancelButton;
+    @FXML
+    private ImageView brandingImageView;
+    @FXML
+    private ImageView lockImageView;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private TextField enterPasswordField;
+
+    //Arreglando el bug de las imagenes
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        File brandingFile = new File("img/homeflix.jpg");
+        Image brandingImage = new Image(brandingFile.toURI().toString());
+        brandingImageView.setImage(brandingImage);
+
+        File lockFile = new File("img/lock.png");
+        Image lockImage = new Image(lockFile.toURI().toString());
+        lockImageView.setImage(lockImage);
+
+    }
+
+    //Validacion
+    public void loginButtonOnAction(ActionEvent event) {
+        if (usernameTextField.getText().isBlank() == false && enterPasswordField.getText().isBlank() == false) {
+            validarLogin();
+        } else {
+            loginMessageLabel.setText("Por favor ingrese su ID y/o PW");
+        }
+    }
+
+
+    public void cancelButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+    public void validarLogin() {
+
+    }
+}
---
+++ src/main/java/com/example/demo/Main.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setTitle("Ingresar");
+        primaryStage.setScene(new Scene(root, 520, 400));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
