c29b5d54e9a3d47d5c2da3cd192a8a0d56e9d3c2
fbnmlnss <fmilaness1@ucenfotec.ac.cr>
2022-07-15 08:24:05 -0600

    se creó la clase UsuarioView y el archivo fxml registrarUsuario

---
+++ src/main/java/View/UsuarioView.java
@@ -0,0 +1,28 @@
+package View;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class UsuarioView {
+    @FXML
+    public Button btnRegistrarUsuario;
+    @FXML
+    public TextField txtNombre;
+    @FXML
+    public TextField txtApellido;
+    @FXML
+    public TextField txtNombreUsuario;
+    @FXML
+    public TextField txtId;
+    @FXML
+    public TextField txtContrasenna;
+    @FXML
+    public TextField txtArchivoImagen;
+
+    @FXML
+    public void handleButtonRegistrarUsuario(ActionEvent event) {
+    }
+
+}
