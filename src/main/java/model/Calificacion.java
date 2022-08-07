20e32842c358da56c4c4d35a0a198b61bf062447
fbnmlnss <fmilaness1@ucenfotec.ac.cr>
2022-08-07 08:00:46 -0600

    Calificacion en proceso

--- src/main/java/model/Calificacion.java
+++ src/main/java/model/Calificacion.java
@@ -1,6 +1,57 @@
 package model;
 
 public class Calificacion {
-    private int videoId;
-    private int userId;
+    private int idCalificacion;
+    private int idVideo;
+    private int idUsuario;
+    private boolean estado;
+
+    public Calificacion(int idCalificacion, int idVideo, int idUsuario, boolean estado) {
+        this.idCalificacion = idCalificacion;
+        this.idVideo = idVideo;
+        this.idUsuario = idUsuario;
+        this.estado = estado;
+    }
+
+    public int getIdCalificacion() {
+        return idCalificacion;
+    }
+
+    public void setIdCalificacion(int idCalificacion) {
+        this.idCalificacion = idCalificacion;
+    }
+
+    public int getIdVideo() {
+        return idVideo;
+    }
+
+    public void setIdVideo(int idVideo) {
+        this.idVideo = idVideo;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Calificacion{" +
+                "idCalificacion=" + idCalificacion +
+                ", idVideo=" + idVideo +
+                ", idUsuario=" + idUsuario +
+                ", estado=" + estado +
+                '}';
+    }
 }
